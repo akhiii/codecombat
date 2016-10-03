19c8e2792a36398ce88632e8dda2afb89dc04693
akhiii <get27jaja@gmail.com>
2016-10-03 05:07:45 +0200

    signs-and-portents.py

---
+++ Dungeon/signs-and-portents.py
@@ -0,0 +1,13 @@
+# https://codecombat.com/play/level/signs-and-portents
+
+hero.moveRight()
+hero.moveRight()
+hero.moveUp()
+hero.moveRight()
+hero.moveRight()
+hero.moveRight()
+hero.moveDown()
+hero.moveRight()
+hero.moveDown()
+hero.moveRight()
+hero.moveRight()
