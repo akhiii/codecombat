2512d75e532ecf4b362e1ad26723e09c2f9ab5a9
akhiii <get27jaja@gmail.com>
2016-10-03 05:05:46 +0200

    forgetful-gemsmith.py

---
+++ Dungeon/forgetful-gemsmith.py
@@ -0,0 +1,10 @@
+# https://codecombat.com/play/level/forgetful-gemsmith
+
+hero.moveRight()
+hero.moveUp()
+hero.moveRight()
+hero.moveRight()
+hero.moveDown()
+hero.moveDown()
+hero.moveUp()
+hero.moveRight()
